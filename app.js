ce1a48bd689e88c8c652cb8131f77bff9043fb79
oussamato98 <oussamaaouad98@gmail.com>
2023-06-24 15:44:38 +0100

    DataValidation + Update + Delete + Embedding Document

--- app.js
+++ app.js
@@ -6,23 +6,156 @@ mongoose.connect("mongodb://127.0.0.1:27017/fruitsdb",{ useNewUrlParser: true});
 
 // On cree une schema pour montrer comment les donnees d une collection particuliere sont structurees
 const fruitSchema = new mongoose.Schema({
-  name: String,
+  //Data Validation
+  name : {
+    type: String
+  //  required: true
+  } ,
   rating : Number ,
   review : String
 });
 
+const personSchema = new mongoose.Schema({
+  name : {
+    type: String
+  } ,
+  age : Number ,
+  //Embedded document
+  favouriteFruit : fruitSchema 
+});
+
+
 
 // we use the schema to create a Mongoose model. 
 const Fruit = mongoose.model("Fruit", fruitSchema);
+const Person= mongoose.model("Person", personSchema);
+
 
 // create a new Fruit Document : 
-const fruit = new Fruit({
+const apple = new Fruit({
     name:"Apple",
     rating:7,
     review:"pretty solid out sl"
 });
+const orange = new Fruit({
+  name:"orange",
+  rating:4,
+  review:"solid out lmsop"
+});
+const pin = new Fruit({
+  
+  rating:4,
+  review:"solid out lmsop"
+});
 
-fruit.save();
+// create a new Person Document : 
+const john = new Person({
+  name:"john",
+  age:17
+});
+
+//john.save();
+
+
+// Fruit.insertMany([apple,orange]);
+
+//pin.save();
+
+//const code =  Fruit.find({});
+
+// async function fetchFruits() {
+//   try {
+//     const fruits = await Fruit.find({});
+//     mongoose.connection.close();
+//     fruits.forEach(function (fruit) {
+//       console.log(fruit);
+//     });
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// fetchFruits();
+
+
+
+// async function updateFruits() {
+//   try {
+//     const res = await Fruit.updateOne(
+//       { _id: "6496f0d4d168b0859d605e06" },
+//       { $set: { name: "USS Enterprise" } }
+//     );
+//     console.log("Modified");
+//     mongoose.connection.close();
+
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// updateFruits();
+
+
+
+// async function deleteFruits() {
+//   try {
+//     const res = await Fruit.deleteOne({ name:"Apple" }); // returns {deletedCount: 1}
+//     console.log("deleted");
+//     mongoose.connection.close();
+
+//   } catch (err) {
+//     console.error(err);
+//   }
+// }
+
+// deleteFruits();
+
+
+
+// Embedding Document :
+
+const pineApple = new Fruit({
+  name:"pineApple",
+  rating:7,
+  review:"pretty solid out sl"
+});
+
+pineApple.save();
+
+
+const david = new Person({
+  name:"david",
+  age:17,
+  favouriteFruit : pineApple 
+});
+
+david.save();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
